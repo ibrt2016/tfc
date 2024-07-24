1dce3cda25204a0308dc8e55387d085642e9bdfe
Ibrahim Tayara <ibrt2012@gmail.com>
2024-07-23 20:43:57 -0400

    add tf files

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "azure_region" {
   description = "Azure region to use"
   type        = string
-  default = "us-east"
+  default     = "us-east"
 }
 
 variable "client_name" {
   description = "Client name/account used in naming"
   type        = string
-  default = "client"
+  default     = "client"
 }
 
 variable "environment" {
   description = "Project environment"
   type        = string
-  default = "dev"
+  default     = "dev"
 }
 
 variable "stack" {
   description = "Project stack name"
   type        = string
-  default = "stack"
+  default     = "stack"
 }
